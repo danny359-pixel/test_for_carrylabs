c5e233f4c9cca2a8c0b67042ef51413f0ea621da
Chauge01 <danny359131763@gmail.com>
2026-06-10 19:12:39 +0800

    Add homepage preview note

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -24,6 +24,13 @@ export default function HomePage() {
           <p className="home-kicker">Carry Labs 教學目錄</p>
           <h1 id="home-title">交易所註冊教學</h1>
           <p className="home-intro">選擇你要查看的交易所教學。目前 HOYA BIT 已完成，其它教學會陸續補上。</p>
+          <p className="home-preview-note">
+            這個分類頁沒有要做～ 單純只是為了方便我們自己預覽而已
+            <br />
+            我們需要的是在「教學文章」這個下拉選單裡面套用之前的分類頁邏輯
+            <br />
+            新增一個頁面叫做「交易所註冊教學」 感恩感恩！！
+          </p>
         </div>
       </section>
 
